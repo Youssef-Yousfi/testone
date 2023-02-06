927f4962ae53f4879efbd8095adc4b20fff16a64
unknown <y.yousfi@aui.ma>
2023-02-06 23:17:01 +0100

    first commit

---
+++ src/pages/Hello.js
@@ -0,0 +1,21 @@
+const Hello = () => {
+    return (
+        <>
+
+            <head>
+                <title>Hello World</title>
+            </head>
+                <h1>Hello World</h1>
+                <p>this is my first Next App</p>
+                <br></br>
+                <a href="http://localhost:3000/">
+                    <h1 style={{fontFamily:"fantasy"}}>Return To Main Page</h1>
+                </a>
+                
+                
+                <img src="011.png" width={200} height={200}></img>
+        </>
+    )
+} 
+
+export default Hello;
